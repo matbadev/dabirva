1697be05cb95e30a9f0f65d5b8593701e839ef5d
Matthias Bäuerle <Ma.Baeuerle@gmx.de>
2021-10-19 22:55:10 +0200

    Fix test

--- dabirva/src/test/java/com/matbadev/dabirva/util/DabirvaConfigTest.kt
+++ dabirva/src/test/java/com/matbadev/dabirva/util/DabirvaConfigTest.kt
@@ -3,11 +3,8 @@ package com.matbadev.dabirva.util
 import com.matbadev.dabirva.Dabirva
 import com.matbadev.dabirva.DabirvaConfig
 import com.matbadev.dabirva.DabirvaFactory
-import com.matbadev.dabirva.ItemViewModel
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertIterableEquals
-import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -51,15 +48,6 @@ class DabirvaConfigTest {
 
     @Nested
     inner class Factory {
-
-        @Test
-        fun `GIVEN factory not changed WHEN get factory EXPECT default factory`() {
-            val factory = DabirvaConfig.factory
-
-            val dabirva: Dabirva = factory.create()
-            assertIterableEquals(listOf<ItemViewModel>(), dabirva.items)
-            assertNull(dabirva.diffExecutor)
-        }
 
         @Test
         fun `WHEN update factory EXPECT new factory applied`() {
