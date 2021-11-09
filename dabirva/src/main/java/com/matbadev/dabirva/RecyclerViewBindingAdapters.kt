fe82c3db7683bf4d8b814826e68b0441a7bdbe4d
Matthias Bäuerle <ma.baeuerle@gmx.de>
2021-11-09 22:10:34 +0100

    Optimize RecyclerView decorations diffing (#33)

--- dabirva/src/main/java/com/matbadev/dabirva/RecyclerViewBindingAdapters.kt
+++ dabirva/src/main/java/com/matbadev/dabirva/RecyclerViewBindingAdapters.kt
@@ -2,14 +2,14 @@ package com.matbadev.dabirva
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.matbadev.dabirva.internal.RecyclerViewDecorationUpdater
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 
 object RecyclerViewBindingAdapters {
 
     @JvmStatic
     @BindingAdapter("itemDecorations")
-    fun setItemDecorations(recyclerView: RecyclerView, itemDecorations: List<RecyclerView.ItemDecoration>?) {
-        RecyclerViewDecorationUpdater.updateDecorations(recyclerView, itemDecorations ?: listOf())
+    fun setItemDecorations(recyclerView: RecyclerView, itemDecorations: List<ItemDecoration>?) {
+        recyclerView.decorations = itemDecorations ?: listOf()
     }
 
 }
---
+++ dabirva/src/main/java/com/matbadev/dabirva/RecyclerViewExtensions.kt
@@ -0,0 +1,25 @@
+package com.matbadev.dabirva
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+
+var RecyclerView.decorations: List<ItemDecoration>
+    get() {
+        val decorationsCount: Int = itemDecorationCount
+        val decorations = mutableListOf<ItemDecoration>()
+        for (decorationIndex in 0 until decorationsCount) {
+            decorations += getItemDecorationAt(decorationIndex)
+        }
+        return decorations
+    }
+    set(newDecorations) {
+        val existingDecorations: List<ItemDecoration> = decorations
+        if (existingDecorations != newDecorations) {
+            existingDecorations.forEach { decoration: ItemDecoration ->
+                removeItemDecoration(decoration)
+            }
+            newDecorations.forEach { decoration: ItemDecoration ->
+                addItemDecoration(decoration)
+            }
+        }
+    }
