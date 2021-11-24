dfda4f2c3fd36d8fc115b144d56c6eae5dcc1c65
Matthias Bäuerle <Ma.Baeuerle@gmx.de>
2021-11-24 21:11:31 +0100

    Improve example

--- example/src/main/java/com/matbadev/dabirva/example/ui/ClickableNoteViewModel.kt
+++ example/src/main/java/com/matbadev/dabirva/example/ui/ClickableNoteViewModel.kt
@@ -1,15 +1,20 @@
 package com.matbadev.dabirva.example.ui
 
+import com.matbadev.dabirva.ItemViewModel
+import com.matbadev.dabirva.example.BR
 import com.matbadev.dabirva.example.R
-import com.matbadev.dabirva.example.base.BaseItemViewModel
 import com.matbadev.dabirva.example.util.ItemClickListener
 
 data class ClickableNoteViewModel(
     val id: Long,
     val text: String,
-) : BaseItemViewModel() {
+) : ItemViewModel {
 
-    override val layoutId: Int = R.layout.item_note_clickable
+    override val bindingId: Int
+        get() = BR.viewModel
+
+    override val layoutId: Int
+        get() = R.layout.item_note_clickable
 
     lateinit var onItemClick: ItemClickListener<ClickableNoteViewModel>
 
--- example/src/main/java/com/matbadev/dabirva/example/ui/NoteRowViewModel.kt
+++ example/src/main/java/com/matbadev/dabirva/example/ui/NoteRowViewModel.kt
@@ -1,16 +1,21 @@
 package com.matbadev.dabirva.example.ui
 
 import androidx.annotation.ColorInt
+import com.matbadev.dabirva.ItemViewModel
+import com.matbadev.dabirva.example.BR
 import com.matbadev.dabirva.example.R
-import com.matbadev.dabirva.example.base.BaseItemViewModel
 
 data class NoteRowViewModel(
     val id: Long,
     val text: String,
     @ColorInt val color: Int,
-) : BaseItemViewModel() {
+) : ItemViewModel {
 
-    override val layoutId: Int = R.layout.item_note_row
+    override val bindingId: Int
+        get() = BR.viewModel
+
+    override val layoutId: Int
+        get() = R.layout.item_note_row
 
     override fun entityEquals(other: Any?): Boolean {
         return other is NoteRowViewModel && other.id == id
