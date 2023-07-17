767fd89330be7361a339a699718e4a3d3f840003
Matthias Bäuerle <ma.baeuerle@gmx.de>
2023-07-17 11:10:51 +0200

    Run tests with JUnit 4 instead of third-party plugin (#59)
    * Increase dependency versions
    
    * Update CI
    
    * Fix lints
    
    * Fix Kotlin version
    
    * Fix Kotlin version
    
    * Fix CI
    
    * Use JUnit4
    
    * Fix DabirvaConfigTest
    
    * Fix coverage
    
    * Simplfy test task

--- dabirva/src/test/java/com/matbadev/dabirva/util/DabirvaConfigTest.kt
+++ dabirva/src/test/java/com/matbadev/dabirva/util/DabirvaConfigTest.kt
@@ -3,52 +3,41 @@ package com.matbadev.dabirva.util
 import com.matbadev.dabirva.Dabirva
 import com.matbadev.dabirva.DabirvaConfig
 import com.matbadev.dabirva.DabirvaFactory
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
 
 class DabirvaConfigTest {
 
-    @BeforeEach
+    @Before
     fun resetConfig() {
         DabirvaConfig.reset()
     }
 
-    @Nested
-    inner class Locking {
-
-        @Test
-        fun `GIVEN config not changed EXPECT not locked`() {
-            assertFalse(DabirvaConfig.locked)
-        }
-
-        @Test
-        fun `GIVEN config locked WHEN update config EXPECT exception`() {
-            val factory = DabirvaFactory { Dabirva() }
-            DabirvaConfig.lock()
-
-            assertThrows(IllegalStateException::class.java) {
-                DabirvaConfig.factory = factory
-            }
-        }
-
+    @Test
+    fun `GIVEN config not changed EXPECT not locked`() {
+        assertFalse(DabirvaConfig.locked)
     }
 
-    @Nested
-    inner class Factory {
+    @Test
+    fun `GIVEN config locked WHEN update config EXPECT exception`() {
+        val factory = DabirvaFactory { Dabirva() }
+        DabirvaConfig.lock()
 
-        @Test
-        fun `WHEN update factory EXPECT new factory applied`() {
-            val factory = DabirvaFactory { Dabirva() }
-
+        assertFailsWith<IllegalStateException> {
             DabirvaConfig.factory = factory
-
-            assertEquals(factory, DabirvaConfig.factory)
         }
+    }
 
+    @Test
+    fun `WHEN update factory EXPECT new factory applied`() {
+        val factory = DabirvaFactory { Dabirva() }
+
+        DabirvaConfig.factory = factory
+
+        assertEquals(factory, DabirvaConfig.factory)
     }
 
 }
